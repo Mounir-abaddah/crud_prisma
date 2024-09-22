b9010b500638755f88fdc6cbbf480d73cd83be71
Mounir abaddah <mounirabaddah6@gmail.com>
2024-09-22 13:59:36 +0100

    feat: Ajout de la fonction GET pour récupérer les informations de l'utilisateur par ID avec ses images

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,6 +1,29 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+
+
+export async function GET(req, { params }) {
+  const userId = Number(params.id);  // Obtenir l'ID de l'URL
+
+  try {
+    // Récupérer l'utilisateur avec ses images
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { images: true },  // Inclure les images de l'utilisateur
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'Utilisateur non trouvé' }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des informations de l\'utilisateur:', error);
+    return NextResponse.json({ error: 'Erreur lors de la récupération des informations de l\'utilisateur' }, { status: 500 });
+  }
+}
+
 export async function PUT(req, { params }) {
   const { name, email, imageUrl } = await req.json();
   const userId = Number(params.id);
